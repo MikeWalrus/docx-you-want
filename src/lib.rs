7da1ff5ff6abbb9627defa6c02d2464ac5d6ede0
Liao Junxuan <mikeljx@126.com>
2022-01-23 22:42:05 +0800

    format and fix some warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,28 @@
 /* This file is part of docx-you-want.
 
-    docx-you-want is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   docx-you-want is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    docx-you-want is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   docx-you-want is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with docx-you-want.  If not, see <https://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with docx-you-want.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #![recursion_limit = "512"]
 
-use tempfile::{TempDir};
-use std::path::{Path, PathBuf};
-use std::fs::{copy, read_to_string, write, remove_file};
 use std::ffi::OsStr;
-use usvg;
-use zip_extensions::zip_create_from_directory;
-use std::process::Command;
+use std::fs::{copy, read_to_string, remove_file, write};
 use std::io::{self, ErrorKind, Write};
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use tempfile::TempDir;
+use zip_extensions::zip_create_from_directory;
 
 #[derive(Debug)]
 pub enum Error {
@@ -84,13 +83,17 @@ fn read_svg(src: &Path) -> Result<usvg::Tree> {
 fn save_png(dst: &Path, rtree: &usvg::Tree) -> Result<()> {
     let size = rtree.svg_node().size.to_screen_size();
     let mut pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
-    resvg::render(&rtree, usvg::FitTo::Original, pixmap.as_mut()).ok_or(Error::ImageError)?;
+    resvg::render(rtree, usvg::FitTo::Original, pixmap.as_mut()).ok_or(Error::ImageError)?;
     pixmap.save_png(dst)?;
     Ok(())
 }
 
 fn get_png_path(prefix: &Path, svg_path: &Path) -> Result<PathBuf> {
-    let filename = svg_path.file_name().unwrap().to_str().ok_or(Error::IoError)?
+    let filename = svg_path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .ok_or(Error::IoError)?
         .replace("svg", "png");
     Ok(prefix.join(Path::new(&filename)))
 }
@@ -106,16 +109,30 @@ pub struct Docx {
     size: usvg::Size,
 }
 
-
 impl Docx {
     pub fn new() -> Result<Docx> {
         let dir = TempDir::new()?;
         Docx::copy_base_files(&dir)?;
         let path = dir.path();
-        let doc: PathBuf = [path.as_os_str(), OsStr::new("word/document.xml")].iter().collect();
-        let rels: PathBuf = [path.as_os_str(), OsStr::new("word/_rels/document.xml.rels")].iter().collect();
-        let media_dir = [path.as_os_str(), OsStr::new("word/media")].iter().collect();
-        Ok(Docx { dir, media_dir, doc, rels, next_id: 0, doc_string: String::new(), rels_string: String::new(), size: usvg::Size::new(793.707, 1122.52).unwrap() })
+        let doc: PathBuf = [path.as_os_str(), OsStr::new("word/document.xml")]
+            .iter()
+            .collect();
+        let rels: PathBuf = [path.as_os_str(), OsStr::new("word/_rels/document.xml.rels")]
+            .iter()
+            .collect();
+        let media_dir = [path.as_os_str(), OsStr::new("word/media")]
+            .iter()
+            .collect();
+        Ok(Docx {
+            dir,
+            media_dir,
+            doc,
+            rels,
+            next_id: 0,
+            doc_string: String::new(),
+            rels_string: String::new(),
+            size: usvg::Size::new(793.707, 1122.52).unwrap(),
+        })
     }
 
     fn copy_base_files(dir: &TempDir) -> Result<()> {
@@ -132,9 +149,10 @@ impl Docx {
         let tree = read_svg(svg)?;
         let png = get_png_path(&self.media_dir, svg)?;
         save_png(&png, &tree)?;
-        let svg_copy = &self.media_dir.join(Path::new(svg.file_name().ok_or(Error::IoError)?));
-        if svg != svg_copy
-        {
+        let svg_copy = &self
+            .media_dir
+            .join(Path::new(svg.file_name().ok_or(Error::IoError)?));
+        if svg != svg_copy {
             copy(svg, svg_copy)?;
         }
         self.add_to_doc(svg_copy, &png, &tree.svg_node().size);
@@ -156,69 +174,77 @@ impl Docx {
         let png_rid = format!("rId{}", png_id);
         let width = px_to_emu(size.width());
         let height = px_to_emu(size.height());
-        self.doc_string = format!("{}{}", self.doc_string, format_xml::xml! {
-          <w:p>
-            <w:pPr>
-                <w:widowControl/>
-                <w:jc w:val="left"/>
-            </w:pPr>
-            <w:r>
-                <w:rPr>
-                    <w:noProof/>
-                </w:rPr>
-                <w:drawing>
-                    <wp:inline distT="0" distB="0" distL="0" distR="0">
-                        <wp:extent cx={width} cy={height}/>
-                        <wp:effectExtent l="0" t="0" r="0" b="0"/>
-                        <wp:docPr id={svg_id} name={svg_id}/>
-                        <wp:cNvGraphicFramePr>
-                            <a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" noChangeAspect="1"/>
-                        </wp:cNvGraphicFramePr>
-                        <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
-                            <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
-                                <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
-                                    <pic:nvPicPr>
-                                        <pic:cNvPr id="1" name=""/>
-                                        <pic:cNvPicPr/>
-                                    </pic:nvPicPr>
-                                    <pic:blipFill>
-                                        <a:blip r:embed={png_rid}>
-                                            <a:extLst>
-                                                <a:ext uri="{{96DAC541-7B7A-43D3-8B79-37D633B846F1}}">
-                                                    <asvg:svgBlip xmlns:asvg="http://schemas.microsoft.com/office/drawing/2016/SVG/main" r:embed={svg_rid}/>
-                                                </a:ext>
-                                            </a:extLst>
-                                        </a:blip>
-                                        <a:stretch>
-                                            <a:fillRect/>
-                                        </a:stretch>
-                                    </pic:blipFill>
-                                    <pic:spPr>
-                                        <a:xfrm>
-                                            <a:off x="0" y="0"/>
-                                            <a:ext cx={width} cy={height}/>
-                                        </a:xfrm>
-                                        <a:prstGeom prst="rect">
-                                            <a:avLst/>
-                                        </a:prstGeom>
-                                    </pic:spPr>
-                                </pic:pic>
-                            </a:graphicData>
-                        </a:graphic>
-                    </wp:inline>
-                </w:drawing>
-            </w:r>
-          </w:p>
-        });
+        self.doc_string = format!(
+            "{}{}",
+            self.doc_string,
+            format_xml::xml! {
+              <w:p>
+                <w:pPr>
+                    <w:widowControl/>
+                    <w:jc w:val="left"/>
+                </w:pPr>
+                <w:r>
+                    <w:rPr>
+                        <w:noProof/>
+                    </w:rPr>
+                    <w:drawing>
+                        <wp:inline distT="0" distB="0" distL="0" distR="0">
+                            <wp:extent cx={width} cy={height}/>
+                            <wp:effectExtent l="0" t="0" r="0" b="0"/>
+                            <wp:docPr id={svg_id} name={svg_id}/>
+                            <wp:cNvGraphicFramePr>
+                                <a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" noChangeAspect="1"/>
+                            </wp:cNvGraphicFramePr>
+                            <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+                                <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
+                                    <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
+                                        <pic:nvPicPr>
+                                            <pic:cNvPr id="1" name=""/>
+                                            <pic:cNvPicPr/>
+                                        </pic:nvPicPr>
+                                        <pic:blipFill>
+                                            <a:blip r:embed={png_rid}>
+                                                <a:extLst>
+                                                    <a:ext uri="{{96DAC541-7B7A-43D3-8B79-37D633B846F1}}">
+                                                        <asvg:svgBlip xmlns:asvg="http://schemas.microsoft.com/office/drawing/2016/SVG/main" r:embed={svg_rid}/>
+                                                    </a:ext>
+                                                </a:extLst>
+                                            </a:blip>
+                                            <a:stretch>
+                                                <a:fillRect/>
+                                            </a:stretch>
+                                        </pic:blipFill>
+                                        <pic:spPr>
+                                            <a:xfrm>
+                                                <a:off x="0" y="0"/>
+                                                <a:ext cx={width} cy={height}/>
+                                            </a:xfrm>
+                                            <a:prstGeom prst="rect">
+                                                <a:avLst/>
+                                            </a:prstGeom>
+                                        </pic:spPr>
+                                    </pic:pic>
+                                </a:graphicData>
+                            </a:graphic>
+                        </wp:inline>
+                    </w:drawing>
+                </w:r>
+              </w:p>
+            }
+        );
         self.add_relationship(&svg_rid, get_filename(svg));
         self.add_relationship(&png_rid, get_filename(png))
     }
 
     fn add_relationship(&mut self, rid: &str, filename: &str) {
         let target = format!("media/{}", filename);
-        self.rels_string = format!("{}{}", self.rels_string, format_xml::xml! {
-            <Relationship Id={rid} Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target={target}/>
-        })
+        self.rels_string = format!(
+            "{}{}",
+            self.rels_string,
+            format_xml::xml! {
+                <Relationship Id={rid} Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target={target}/>
+            }
+        )
     }
 
     pub fn generate_docx(self, p: &PathBuf) -> Result<()> {
@@ -236,8 +262,14 @@ impl Docx {
 
     fn change_size(&self) -> Result<()> {
         let s = read_to_string(&self.doc)?
-            .replace("!WIDTH!", &px_to_twenties_of_pt(self.size.width()).to_string())
-            .replace("!HEIGHT!", &px_to_twenties_of_pt(self.size.height()).to_string());
+            .replace(
+                "!WIDTH!",
+                &px_to_twenties_of_pt(self.size.width()).to_string(),
+            )
+            .replace(
+                "!HEIGHT!",
+                &px_to_twenties_of_pt(self.size.height()).to_string(),
+            );
         write(&self.doc, s)?;
         Ok(())
     }
@@ -255,19 +287,22 @@ impl Docx {
         loop {
             page += 1;
             let image = PathBuf::from(&self.media_dir).join(format! {"{}.svg", page});
-            let output = match
-            Command::new("inkscape")
+            let output = match Command::new("inkscape")
                 .arg(pdf)
                 .arg(format!("--pdf-page={}", page))
-                .arg("-o").arg(&image)
+                .arg("-o")
+                .arg(&image)
                 .arg("--pdf-poppler")
-                .output() {
+                .output()
+            {
                 Err(e) => {
                     return if let ErrorKind::NotFound = e.kind() {
                         Err(Error::InkscapeNotFound)
-                    } else { Err(Error::IoError) };
+                    } else {
+                        Err(Error::IoError)
+                    };
                 }
-                Ok(output) => (output)
+                Ok(output) => (output),
             };
             print!(".");
             io::stdout().flush()?;
@@ -280,14 +315,15 @@ impl Docx {
             break;
         }
         print!("Getting the size of the first page ... ");
-        self.size = read_svg(&images.get(0).ok_or(Error::PDFInvalid)?)?.svg_node().size;
+        self.size = read_svg(images.get(0).ok_or(Error::PDFInvalid)?)?
+            .svg_node()
+            .size;
         println!("Done.");
         print!("Adding all the images ");
         io::stdout().flush()?;
-        images.iter().map(|i| self.add_image_svg(&i)).collect()
+        images.iter().try_for_each(|i| self.add_image_svg(i))
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -301,20 +337,19 @@ mod tests {
     }
 
     #[test]
-    fn test_dir() -> Result<()>
-    {
+    fn test_dir() -> Result<()> {
         let docx = Docx::new().unwrap();
         let dir = docx.dir.path();
         assert!(dir.exists());
-        let children = get_children(&dir)?;
+        let children = get_children(dir)?;
         let mut children_str: Vec<&str> = children
             .iter()
             .map(|i| i.file_name().unwrap().to_str().unwrap())
             .collect();
         let mut result = vec!["word", "[Content_Types].xml", "_rels"];
 
-        children_str.sort();
-        result.sort();
+        children_str.sort_unstable();
+        result.sort_unstable();
         assert_eq!(children_str, result);
         Ok(())
     }
@@ -333,7 +368,6 @@ mod tests {
         let tests_dir = String::from(env!("CARGO_MANIFEST_DIR")) + "/tests/";
         PathBuf::from(format!("{}{}", tests_dir, "2.svg"))
     }
-
 
     fn get_tests_dir() -> String {
         String::from(env!("CARGO_MANIFEST_DIR")) + "/tests/"
@@ -416,7 +450,8 @@ mod tests {
     fn test_generate_docx() {
         let mut docx = Docx::new().unwrap();
         docx.add_image_svg(&get_test_svg()).unwrap();
-        docx.generate_docx(&PathBuf::from(get_tests_dir() + "a.docx")).unwrap();
+        docx.generate_docx(&PathBuf::from(get_tests_dir() + "a.docx"))
+            .unwrap();
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 /* This file is part of docx-you-want.
 
-    docx-you-want is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   docx-you-want is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    docx-you-want is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   docx-you-want is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with docx-you-want.  If not, see <https://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with docx-you-want.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
+use docx_you_want as dyw;
+use docx_you_want::Error;
 use std::env::args;
 use std::io::{self, Write};
 use std::path::Path;
-use docx_you_want as dyw;
-use docx_you_want::Error;
 use std::process::exit;
-
 
 fn main() {
     let args: Vec<_> = args().collect();
     if args.len() != 3 {
-        println!("Usage: {} <path to PDF> <path to result DOCX file>", args[0]);
+        println!(
+            "Usage: {} <path to PDF> <path to result DOCX file>",
+            args[0]
+        );
         exit(-1)
     }
     let src = Path::new(&args[1]);
@@ -35,7 +37,7 @@ fn main() {
             Error::IoError => "An error occurred during I/O.",
             Error::ImageError => "Something went wrong while processing the images.",
             Error::InkscapeNotFound => "Inkscape not found. Consider installing inkscape?",
-            Error::PDFInvalid => "Invalid PDF."
+            Error::PDFInvalid => "Invalid PDF.",
         };
         eprint!("{}", msg);
         exit(-1);
